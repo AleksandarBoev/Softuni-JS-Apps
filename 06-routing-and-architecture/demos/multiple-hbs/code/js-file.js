0af8bc0f09ace68ffa272079451cd9befa3073f0
AleksandarBoev <aleksandarboev95@gmail.com>
2019-08-04 08:55:57 +0300

    Made some minor changes

--- 06-routing-and-architecture/demos/multiple-hbs/code/js-file.js
+++ 06-routing-and-architecture/demos/multiple-hbs/code/js-file.js
@@ -11,15 +11,21 @@ const sammyApp = Sammy('#root', function () {
         });
     });
 
+    this.get('#/random_page_2', function() {
+        this.loadPartials({
+            firstPartial: './templates/header.hbs',
+            secondPartial: './templates/random2.hbs',
+            thirdPartial: './templates/footer.hbs'
+        }).then(function () {
+            this.partial('./templates/page-template.hbs');
+        });
+    });
+
     this.get('#/goodbye', function() {
         console.log('Noo goodbye, only hello!');
         this.redirect('#/hello');
+        console.log('If you see this, then "redirect" does NOT stop the rest of the function code.');
     });
-
-    this.get('', function() {
-        this.redirect('#/hello');
-        console.log('Welcome to the app!');
-    })
 });
 
 (() => sammyApp.run('#/hello'))();
